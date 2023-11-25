ef1a2c560f80fc7f47d505de9d67336884f52954
chiar15 <chiara.ferraioli02@gmail.com>
2023-11-25 13:32:43 +0100

    Initial packages structure

---
+++ AutomationApp/src/it/unisa/diem/se/automationapp/trigger/TriggerInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.unisa.diem.se.automationapp.trigger;
+
+/**
+ *
+ * @author chiar
+ */
+public interface TriggerInterface {
+    
+}
